931a1d906c9e9da4e76a0f7096b810e0d033ab34
Abel Goodwin <abelgoodwin1988@gmail.com>
2019-04-12 17:44:12 -0600

    remove working code

--- analyze_text.py
+++ analyze_text.py
@@ -2,12 +2,3 @@
 
 def analyze_text(text):
     # Your code here
-    charCount = 0
-    eCount = 0
-    text = text.lower()
-    for char in text:
-        if char.isalpha():
-            charCount+=1
-            if char == 'e':
-                eCount+=1
-    return f"The text contains {charCount} alphabetic characters, of which {eCount} ({100*eCount/charCount:0.2f}%) are 'e'."
