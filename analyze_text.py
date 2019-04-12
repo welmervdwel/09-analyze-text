4fcc42c50e1c455836162ae2c92925c0c375235b
Abel Goodwin <abelgoodwin1988@gmail.com>
2019-04-12 17:22:51 -0600

    functional test suite

--- analyze_text.py
+++ analyze_text.py
@@ -2,3 +2,12 @@
 
 def analyze_text(text):
     # Your code here
+    charCount = 0
+    eCount = 0
+    text = text.lower()
+    for char in text:
+        if char.isalpha():
+            charCount+=1
+            if char == 'e':
+                eCount+=1
+    return f"The text contains {charCount} alphabetic characters, of which {eCount} ({100*eCount/charCount}%) are 'e'."
--- test.py
+++ test.py
@@ -15,8 +15,8 @@ test_cases = [
     },
 ]
 
-def test_leap_year_test_cases():
+def test_analyze_text():
     for test_case in test_cases:
-        analyze_text_response = analyze_text(test_cases['input'])
-        assert is_leap_year_response == test_case[
-            1], f'For Input:\n{test_case["input"]} \mExpected: {test_case["output"]} \nGot: {analyze_text_response}'
+        analyze_text_response = analyze_text(test_case['input'])
+        assert analyze_text_response == test_case[
+            'output'], f"\nFor Input: {test_case['input']} \nExpected: {test_case['output']} \nGot:      {analyze_text_response}"
